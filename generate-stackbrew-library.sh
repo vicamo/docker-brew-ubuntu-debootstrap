ad1f27c48b0a2644997e7e490d49292deed9d2a6
You-Sheng Yang <vicamo@gmail.com>
2015-07-10 12:58:44 +0800

    remove devel suite
    'devel' is a symbolic link to latest development suite, which should
    have been wily now.

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -7,10 +7,6 @@ declare -A aliases
 aliases=(
 	[$(cat latest)]='latest'
 )
-declare -A noVersion
-noVersion=(
-	[devel]=1
-)
 
 versions=( */ )
 versions=( "${versions[@]%/}" )
@@ -29,23 +25,21 @@ fi
 for version in "${versions[@]}"; do
 	commit="$(git log -1 --format='format:%H' "$version")"
 	versionAliases=()
-	if [ -z "${noVersion[$version]}" ]; then
-		fullVersion="$(tar -xvf "$version/rootfs.tar.xz" etc/debian_version --to-stdout 2>/dev/null)"
-		if [ -z "$fullVersion" ] || [[ "$fullVersion" == */sid ]]; then
-			fullVersion="$(eval "$(tar -xvf "$version/rootfs.tar.xz" etc/os-release --to-stdout 2>/dev/null)" && echo "$VERSION" | cut -d' ' -f1)"
-			if [ -z "$fullVersion" ]; then
-				# lucid...
-				fullVersion="$(eval "$(tar -xvf "$version/rootfs.tar.xz" etc/lsb-release --to-stdout 2>/dev/null)" && echo "$DISTRIB_DESCRIPTION" | cut -d' ' -f2)" # DISTRIB_DESCRIPTION="Ubuntu 10.04.4 LTS"
-			fi
-		else
-			while [ "${fullVersion%.*}" != "$fullVersion" ]; do
-				versionAliases+=( $fullVersion )
-				fullVersion="${fullVersion%.*}"
-			done
+	fullVersion="$(tar -xvf "$version/rootfs.tar.xz" etc/debian_version --to-stdout 2>/dev/null)"
+	if [ -z "$fullVersion" ] || [[ "$fullVersion" == */sid ]]; then
+		fullVersion="$(eval "$(tar -xvf "$version/rootfs.tar.xz" etc/os-release --to-stdout 2>/dev/null)" && echo "$VERSION" | cut -d' ' -f1)"
+		if [ -z "$fullVersion" ]; then
+			# lucid...
+			fullVersion="$(eval "$(tar -xvf "$version/rootfs.tar.xz" etc/lsb-release --to-stdout 2>/dev/null)" && echo "$DISTRIB_DESCRIPTION" | cut -d' ' -f2)" # DISTRIB_DESCRIPTION="Ubuntu 10.04.4 LTS"
 		fi
-		if [ "$fullVersion" != "$version" ]; then
+	else
+		while [ "${fullVersion%.*}" != "$fullVersion" ]; do
 			versionAliases+=( $fullVersion )
-		fi
+			fullVersion="${fullVersion%.*}"
+		done
+	fi
+	if [ "$fullVersion" != "$version" ]; then
+		versionAliases+=( $fullVersion )
 	fi
 	versionAliases+=( $version $(cat "$version/suite" 2>/dev/null || true) ${aliases[$version]} )
 	
