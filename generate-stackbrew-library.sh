c7df7d11e1c3539cde7763c85cb406a3d5b8932b
You-Sheng Yang <vicamo@gmail.com>
2015-07-10 14:58:42 +0800

    multiarch support

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -10,6 +10,13 @@ aliases=(
 
 suites=( */ )
 suites=( "${suites[@]%/}" )
+for suite in "${suites[@]}"; do
+	arches=( $suite/*/ )
+	arches=( "${arches[@]%/}" )
+	arches=( "${arches[@]#$suite/}" )
+	eval arches_$suite=\( ${arches[@]} \)
+done
+
 url='git://github.com/tianon/docker-brew-ubuntu-debootstrap'
 
 echo '# maintainer: Tianon Gravi <admwiggin@gmail.com> (@tianon)'
@@ -24,28 +31,38 @@ fi
 
 for suite in "${suites[@]}"; do
 	version="$(cat "$suite/version")"
-	commit="$(git log -1 --format='format:%H' "$suite")"
-	versionAliases=()
-	fullVersion="$(tar -xvf "$suite/rootfs.tar.xz" etc/debian_version --to-stdout 2>/dev/null)"
-	if [ -z "$fullVersion" ] || [[ "$fullVersion" == */sid ]]; then
-		fullVersion="$(eval "$(tar -xvf "$suite/rootfs.tar.xz" etc/os-release --to-stdout 2>/dev/null)" && echo "$VERSION" | cut -d' ' -f1)"
-		if [ -z "$fullVersion" ]; then
-			# lucid...
-			fullVersion="$(eval "$(tar -xvf "$suite/rootfs.tar.xz" etc/lsb-release --to-stdout 2>/dev/null)" && echo "$DISTRIB_DESCRIPTION" | cut -d' ' -f2)" # DISTRIB_DESCRIPTION="Ubuntu 10.04.4 LTS"
+	eval arches=\( \${arches_${version}[@]} \)
+	for arch in "${arches[@]}"; do
+		dir="$suite/$arch"
+		commit="$(git log -1 --format='format:%H' "$dir")"
+		versionAliases=()
+		fullVersion="$(tar -xvf "$dir/rootfs.tar.xz" etc/debian_version --to-stdout 2>/dev/null)"
+		if [ -z "$fullVersion" ] || [[ "$fullVersion" == */sid ]]; then
+			fullVersion="$(eval "$(tar -xvf "$dir/rootfs.tar.xz" etc/os-release --to-stdout 2>/dev/null)" && echo "$VERSION" | cut -d' ' -f1)"
+			if [ -z "$fullVersion" ]; then
+				# lucid...
+				fullVersion="$(eval "$(tar -xvf "$dir/rootfs.tar.xz" etc/lsb-release --to-stdout 2>/dev/null)" && echo "$DISTRIB_DESCRIPTION" | cut -d' ' -f2)" # DISTRIB_DESCRIPTION="Ubuntu 10.04.4 LTS"
+			fi
+		else
+			while [ "${fullVersion%.*}" != "$fullVersion" ]; do
+				versionAliases+=( $fullVersion )
+				fullVersion="${fullVersion%.*}"
+			done
 		fi
-	else
-		while [ "${fullVersion%.*}" != "$fullVersion" ]; do
-			versionAliases+=( $fullVersion )
-			fullVersion="${fullVersion%.*}"
-		done
-	fi
-	if [ "$fullVersion" != "$version" ]; then
-		versionAliases+=( $fullVersion )
-	fi
-	versionAliases+=( $version $suite ${aliases[$suite]} )
+		if [ "$fullVersion" != "$version" ]; then
+			versionAliases+=( $fullVersion-$arch )
+			[ "${arch}" = "amd64" ] && versionAliases+=( $fullVersion )
+		fi
+		versionAliases+=( $version-$arch $suite-$arch )
+		[ "${arch}" = "amd64" ] && versionAliases+=( $version $suite )
+		if [ -n "${aliases[$suite]}" ]; then
+			versionAliases+=( latest-$arch )
+			[ "${arch}" = "amd64" ] && versionAliases+=( latest )
+		fi
 	
-	echo
-	for va in "${versionAliases[@]}"; do
-		echo "$va: ${url}@${commit} $version"
+		echo
+		for va in "${versionAliases[@]}"; do
+			echo "$va: ${url}@${commit} $version"
+		done
 	done
 done
--- update.sh
+++ update.sh
@@ -3,16 +3,10 @@ set -eo pipefail
 
 cd "$(readlink -f "$(dirname "$BASH_SOURCE")")"
 
-suites=( "$@" )
-if [ ${#suites[@]} -eq 0 ]; then
-	suites=( */ )
-fi
-suites=( "${suites[@]%/}" )
-
 get_part() {
-	dir="$1"
+	local dir="$1"
 	shift
-	part="$1"
+	local part="$1"
 	shift
 	if [ -f "$dir/$part" ]; then
 		cat "$dir/$part"
@@ -29,6 +23,17 @@ get_part() {
 	return 1
 }
 
+: ${sudo:=sudo}
+declare -a envs
+# contrib/mkimage.sh
+envs+=(TMPDIR)
+# contrib/mkimage/debootstrap
+envs+=(DEBOOTSTRAP DONT_TOUCH_SOURCES_LIST)
+for var in "${envs[@]}"; do
+	eval value=\$$var
+	[ -z "$value" ] || sudo="$sudo $var=$value"
+done
+
 repo="$(get_part . repo '')"
 if [ "$repo" ]; then
 	if [[ "$repo" != */* ]]; then
@@ -39,26 +44,89 @@ if [ "$repo" ]; then
 	fi
 fi
 
+latest="$(get_part . latest '')"
+
+args=( "$@" )
+if [ ${#args[@]} -eq 0 ]; then
+	args=( */ )
+fi
+
+suites=()
+for arg in "${args[@]}"; do
+	arg=${arg%/}
+	arch=$(echo $arg | cut -d / -f 2)
+	suite=$(echo $arg | cut -d / -f 1)
+	if [ "$arch" == "$suite" ]; then
+		arch=
+	fi
+
+	if [ -z "`echo ${suites[@]} | grep $suite`" ]; then
+		suites+=( $suite )
+	fi
+
+	name=arches_$suite
+	if [ "$arch" ]; then
+		eval arches=\( \${${name}[@]} \)
+		if [ ${#arches[@]} -ne 0 ]; then
+			if [ -z "`echo ${arches[@]} | grep $arch`" ]; then
+				eval $name+=\( "$arch" \)
+			fi
+		else
+			eval $name=\( "$arch" \)
+		fi
+	else
+		arches=( $suite/*/ )
+		arches=( "${arches[@]%/}" )
+		arches=( "${arches[@]#$suite/}" )
+		if [ ${#arches[@]} -lt 0 -o "${arches[0]}" != "*" ]; then
+			eval $name=\( "${arches[@]}" \)
+		fi
+	fi
+
+	#echo "arch: $arch, suite: $suite"
+	#echo "suites: ${suites[@]}"
+	#eval echo "$name: \${${name}[@]}"
+	#echo
+done
+
+tasks=()
 for suite in "${suites[@]}"; do
-	dir="$(readlink -f "$suite")"
+	name=arches_$suite
+	eval arches=\( \${${name}[@]} \)
+	for arch in "${arches[@]}"; do
+		dir="$(readlink -f "$suite/$arch")"
+
+		skip="$(get_part "$dir" skip '')"
+		if [ -n "$skip" ]; then
+			echo "Skipping $suite/$arch, reason: $skip"
+			continue
+		fi
+
+		tasks+=( $suite/$arch )
+	done
+done
+
+for task in "${tasks[@]}"; do
+	suite=$(echo $task | cut -d / -f 1)
+	arch=$(echo $task | cut -d / -f 2)
+	dir="$(readlink -f "$task")"
 	variant="$(get_part "$dir" variant 'minbase')"
 	components="$(get_part "$dir" components 'main')"
 	include="$(get_part "$dir" include '')"
-	version="$(get_part "$dir" version "$suite")"
+	version="$(get_part "$(readlink -f "$suite")" version "$suite")"
 	mirror="$(get_part "$dir" mirror '')"
 	script="$(get_part "$dir" script '')"
-	
-	args=( -d "$dir" debootstrap )
+
+	args=( -d "$dir" debootstrap --arch="$arch" )
 	[ -z "$variant" ] || args+=( --variant="$variant" )
 	[ -z "$components" ] || args+=( --components="$components" )
 	[ -z "$include" ] || args+=( --include="$include" )
-	
-	debootstrapVersion="$(debootstrap --version)"
-	debootstrapVersion="${debootstrapVersion##* }"
+
+	debootstrapVersion="$(dpkg-query -W -f '${Version}' debootstrap)"
 	if dpkg --compare-versions "$debootstrapVersion" '>=' '1.0.69'; then
 		args+=( --force-check-gpg )
 	fi
-	
+
 	args+=( "$suite" )
 	if [ "$mirror" ]; then
 		args+=( "$mirror" )
@@ -66,24 +134,38 @@ for suite in "${suites[@]}"; do
 			args+=( "$script" )
 		fi
 	fi
-	
+
 	mkimage="$(readlink -f "${MKIMAGE:-"mkimage.sh"}")"
 	{
 		echo "$(basename "$mkimage") ${args[*]/"$dir"/.}"
 		echo
 		echo 'https://github.com/docker/docker/blob/master/contrib/mkimage.sh'
 	} > "$dir/build-command.txt"
-	
-	sudo nice ionice -c 3 "$mkimage" "${args[@]}" 2>&1 | tee "$dir/build.log"
-	
-	sudo chown -R "$(id -u):$(id -g)" "$dir"
-	
+
+	${sudo} nice ionice -c 3 "$mkimage" "${args[@]}" 2>&1 | tee "$dir/build.log"
+
+	${sudo} chown -R "$(id -u):$(id -g)" "$dir"
+
 	if [ "$repo" ]; then
-		( set -x && docker build -t "${repo}:${suite}" "$dir" )
-		if [ "$suite" != "$version" ]; then
-			( set -x && docker tag -f "${repo}:${suite}" "${repo}:${version}" )
+		( set -x && docker build -t "${repo}:${suite}-${arch}" "$dir" )
+
+		tags=()
+		[ "${arch}" = "amd64" ] && tags+=( "${suite}" )
+		if [ "$suite" != "${version}" ]; then
+			tags+=( "${version}-${arch}" )
+			[ "${arch}" = "amd64" ] && tags+=( "${version}" )
 		fi
-		docker run -it --rm "${repo}:${suite}" bash -xc '
+		if [ "${suite}" = "${latest}" ]; then
+			tags+=( "latest-${arch}" )
+			[ "${arch}" = "amd64" ] && tags+=( "latest" )
+		fi
+		{
+			set -x
+			for tag in "${tags[@]}"; do
+				docker tag -f "${repo}:${suite}-${arch}" "${repo}:${tag}"
+			done
+		}
+		docker run -it --rm "${repo}:${suite}-${arch}" bash -xc '
 			cat /etc/apt/sources.list
 			echo
 			cat /etc/os-release 2>/dev/null
@@ -93,11 +175,6 @@ for suite in "${suites[@]}"; do
 			cat /etc/debian_version 2>/dev/null
 			true
 		'
-		docker run --rm "${repo}:${suite}" dpkg-query -f '${Package}\t${Version}\n' -W > "$dir/build.manifest"
+		docker run --rm "${repo}:${suite}-${arch}" dpkg-query -f '${Package}\t${Version}\n' -W > "$dir/build.manifest"
 	fi
 done
-
-latest="$(get_part . latest '')"
-if [ "$latest" ]; then
-	( set -x && docker tag -f "${repo}:${latest}" "${repo}:latest" )
-fi
